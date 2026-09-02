f1c290af9e8defd242f68c54764ddf2379d9c81d
BVAndy259 <andicito183@gmail.com>
2026-09-02 18:32:14 -0500

    add register controller (25%)

---
+++ backend/src/controllers/register.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import "multer";
+import { RegisterServices } from "../services/register.service";
+
+export const RegisterController = {
+  async create(req: Request, res: Response) {
+    try {
+      const { game_id, hours_played, registration_date, notes } = req.body;
+
+      if (!game_id || hours_played === undefined || !registration_date) {
+        return res
+          .status(400)
+          .json({
+            error:
+              "Los campos game_id, hours_played y registration_date son obligatorios",
+          });
+      }
+
+      const newRegister = await RegisterServices.createRegister(
+        game_id,
+        hours_played,
+        registration_date,
+        notes,
+      );
+      res
+        .status(201)
+        .json({
+          mensaje: "Horas registradas con éxito",
+          registro: nuevoRegistro,
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error interno al guardar las horas" });
+    }
+  },
+};
