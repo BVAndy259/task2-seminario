ec208854660dc95d7f106109b4952293102c21ae
BVAndy259 <andicito183@gmail.com>
2026-09-04 16:49:16 -0500

    add dotenv

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
-export const API_URL = "http://localhost:3000";
+dotenv.config({ path: ".env.local" });
+
+export const API_URL = process.env.API_URL_BACK || "http://localhost:3000";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
