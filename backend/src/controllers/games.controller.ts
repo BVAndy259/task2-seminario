85ef0c2eb85a6afb70b1bf07c88b60365cfcb6e9
BVAndy259 <andicito183@gmail.com>
2026-09-03 19:24:38 -0500

    add update and delete in games controller

--- backend/src/controllers/games.controller.ts
+++ backend/src/controllers/games.controller.ts
@@ -33,4 +33,61 @@ export const GameController = {
       res.status(500).json({ error: "Error al obtener el listado de juegos" });
     }
   },
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { title, platform, status_game } = req.body;
+
+    if (isNaN(Number(id)))
+      return res.status(400).json({ error: "ID inválido" });
+
+    try {
+      const homepage_url = req.file ? `/uploads/${req.file.filename}` : null;
+
+      const updatedGame = await GameService.updateGame({
+        g_id: Number(id),
+        title,
+        platform,
+        status_game,
+        homepage_url,
+      });
+
+      if (!updatedGame)
+        return res
+          .status(404)
+          .json({ error: "Juego no encontrado para actualizar" });
+
+      res.json({
+        message: "Juego actualizado correctamente",
+        game: updatedGame,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error interno al actualizar el juego" });
+    }
+  },
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    if (isNaN(Number(id)))
+      return res.status(400).json({ error: "ID inválido" });
+
+    try {
+      const deletedGame = await GameService.deleteGame(Number(id));
+
+      if (!deletedGame)
+        return res
+          .status(404)
+          .json({ error: "Juego no encontrado para eliminar" });
+
+      res.json({
+        message:
+          "Juego eliminado correctamente junto con su historial de horas",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error interno al eliminar el juego" });
+    }
+  },
 };
