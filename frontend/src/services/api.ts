4ae1ccdec6893c70112e1a4f4d88500dd727f5f1
BVAndy259 <andicito183@gmail.com>
2026-09-04 18:41:10 -0500

    fix api.ts and create README

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
-
-export const API_URL = process.env.API_URL_BACK || "http://localhost:3000";
+export const API_URL =
+  import.meta.env.VITE_API_URL_BACK || "http://localhost:3000";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
